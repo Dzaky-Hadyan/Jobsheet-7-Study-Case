33e105c370acdc5138defe2e9d86fd01b311e6d0
Adammm <adambahymaulana7@gmail.com>
2025-04-21 15:42:52 +0700

    up sorting

--- Jadwal.java
+++ Jadwal.java
@@ -3,11 +3,29 @@ public class Jadwal {
     MataKuliah matakuliah;
     String hari;
     String jam;
+    int kodeHari;
 
     public Jadwal(Dosen dosen, MataKuliah mataKuliah, String hari, String jam) {
         this.dosen = dosen;
         this.matakuliah = mataKuliah;
         this.hari = hari;
+        if (this.hari.equalsIgnoreCase("Senin")) {
+            kodeHari = 1;
+        } else if (this.hari.equalsIgnoreCase("Selasa")) {
+            kodeHari = 2;
+        } else if (this.hari.equalsIgnoreCase("Rabu")) {
+            kodeHari = 3;
+        } else if (this.hari.equalsIgnoreCase("Kamis")) {
+            kodeHari = 4;
+        } else if (this.hari.equalsIgnoreCase("Jumat")) {
+            kodeHari = 5;
+        } else if (this.hari.equalsIgnoreCase("Sabtu")) {
+            kodeHari = 6;
+        } else if (this.hari.equalsIgnoreCase("Minggu")) {
+            kodeHari = 7;
+        } else {
+            kodeHari = 0; // atau nilai default lain jika input tidak valid
+        }
         this.jam = jam;
     }
 
--- JadwalDosen.java
+++ JadwalDosen.java
@@ -10,4 +10,44 @@ public class JadwalDosen {
             System.out.println("Data sudah penuh");
         }
     }
+
+        void SortingASC() {
+            for (int i = 0; i < idx - 1; i++) {
+                for (int j = 0; j < idx - 1 - i; j++) {
+                    if (listJadwal[j].kodeHari > listJadwal[j + 1].kodeHari) {
+                        Jadwal temp = listJadwal[j];
+                        listJadwal[j] = listJadwal[j + 1];
+                        listJadwal[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    
+        // Selection Sort 
+        // void sortingDSC() {
+        //     for (int i = 0; i < idx - 1; i++) {
+        //         int idxMax = i;
+        //         for (int j = i + 1; j < idx; j++) {
+        //             if (dataDosen[j].usia > dataDosen[idxMax].usia) {
+        //                 idxMax = j;
+        //             }
+        //         }
+        //         Dosen01 temp = dataDosen[idxMax];
+        //         dataDosen[idxMax] = dataDosen[i];
+        //         dataDosen[i] = temp;
+        //     }
+        // }
+    
+        // Insertion Sort 
+        // void insertionSort() {
+        //     for (int i = 1; i < idx; i++) {
+        //         Dosen01 temp = dataDosen[i];
+        //         int j = i - 1;
+        //         while (j >= 0 && dataDosen[j].usia < temp.usia) {
+        //             dataDosen[j + 1] = dataDosen[j];
+        //             j--;
+        //         }
+        //         dataDosen[j + 1] = temp;
+        //     }
+        // }
 }
