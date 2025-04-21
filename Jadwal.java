bd9f820307124a72803df2c85bc89915e3319889
Adammm <adambahymaulana7@gmail.com>
2025-04-21 14:26:08 +0700

    add class jadwal

--- Jadwal.java
+++ Jadwal.java
@@ -1,10 +1,10 @@
 public class Jadwal {
-    DosenMain dosen;
+    Dosen dosen;
     MataKuliah matakuliah;
     String hari;
     String jam;
 
-    public Jadwal(DosenMain dosen, MataKuliah mataKuliah, String hari, String jam) {
+    public Jadwal(Dosen dosen, MataKuliah mataKuliah, String hari, String jam) {
         
     }
 
